e6398bcf83edee198ec1876353cdacceaed5267a
fiGuy.lTd <Code@fiGuy.lTd>
2025-09-14 17:22:58 -0400

    Refactor file globbing in default.py to specify individual part files for improved clarity and control

--- src/01/default.py
+++ src/01/default.py
@@ -3,31 +3,37 @@ from glob import glob
 import os
 import shutil as sh
 
+srcLetr = 'D'
+srcRepo = 'GitHub'
+srcComp = 'fiGuys'
 srcName = 'Identity'
-srcStep = '01'
-srcNext = '02'
-srcGlobs = f'D:\\{srcName}\\src\\{srcStep}\\Part*.csv'
+srcStep = f'{1:02}'
+srcNext = f'{int(srcStep + 1):02}'
+srcRoot = f'{srcLetr}:\\{srcRepo}\\{srcComp}\\{srcName}\\src'
+srcGlobs = f'{srcRoot}\\{srcStep}\\Part*.csv'
 srcFiles = glob(srcGlobs)
 
+
 def flow_dataStreams(srcFile, outFile, newFile):
-    try:
-     print(f'Converting {srcFile} to parquet format.')
-     lf = pl.scan_csv(
+	try:
+		print(f'Converting {srcFile} to parquet format.')
+		lf = pl.scan_csv(
 			srcFile,
 			ignore_errors=True,
 			infer_schema=False,
 		)
-     lf.sink_parquet(outFile)
+		lf.sink_parquet(outFile)
 
-     if os.path.exists(outFile):
-      if os.path.exists(newFile):
-       os.remove(newFile)
+		if os.path.exists(outFile):
+			if os.path.exists(newFile):
+				os.remove(newFile)
 
-      print(f'Staging {newFile}')
-      sh.copy(outFile, newFile)
+			print(f'Staging {newFile}')
+			sh.copy(outFile, newFile)
 
-    except Exception as e:
-     print(f'Error combining Parquet files: {e}')
+	except Exception as e:
+		print(f'Error combining Parquet files: {e}')
+
 
 for srcFile in srcFiles:
 	outFile = srcFile.replace(f'src\\{srcStep}', f'out\\{srcStep}')
