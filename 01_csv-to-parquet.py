49238537b97acc5e47932acea4a1626e122df3ac
Paul <Paul@fiGuy.lTd>
2025-09-14 05:12:26 -0400

    Refactor file path handling for CSV to Parquet conversion

--- 01_csv-to-parquet.py
+++ 01_csv-to-parquet.py
@@ -3,32 +3,31 @@ from glob import glob
 import os
 import shutil as sh
 
-srcGlobs = "D:\\OneLake\\src\\01\\Part*.csv"
+srcName = 'Identity'
+srcStep = '01'
+srcNext = '02'
+srcGlobs = f'D:\\{srcName}\\src\\{srcStep}\\Part*.csv'
 srcFiles = glob(srcGlobs)
 
 for srcFile in srcFiles:
-    outFile = srcFile.replace(".csv", ".parquet").replace("src", "out")
+	outFile = srcFile.replace(f'src\\{srcStep}', f'out\\{srcStep}')
+	newFile = outFile.replace(f'out\\{srcStep}', f'src\\{srcNext}')
 
-    try:
-        print(f"Converting {srcFile} to parquet format.")
-        lf = pl.scan_csv(
-            srcFile,
-            ignore_errors=True,
-            infer_schema=False,
-        )
-        lf.sink_parquet(outFile)
+	try:
+		print(f'Converting {srcFile} to parquet format.')
+		lf = pl.scan_csv(
+			srcFile,
+			ignore_errors=True,
+			infer_schema=False,
+		)
+		lf.sink_parquet(outFile)
 
-        if os.path.exists(outFile):
-            newFile = (
-                srcFile.replace("\\out", "\\src")
-                .replace("\\01", "\\02")
-                .replace(".csv", ".parquet")
-            )
+		if os.path.exists(outFile):
+			if os.path.exists(newFile):
+				os.remove(newFile)
 
-            if os.path.exists(newFile):
-                os.remove(newFile)
-            print(f"Staging {newFile}")
-            sh.copy(outFile, newFile)
+			print(f'Staging {newFile}')
+			sh.copy(outFile, newFile)
 
-    except Exception as e:
-        print(f"Error combining Parquet files: {e}")
+	except Exception as e:
+		print(f'Error combining Parquet files: {e}')
